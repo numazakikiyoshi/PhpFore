f2e5a96e299024df908c34d78a4a024230ebfa98
numazaki <numaki0110@yahoo.co.jp>
2016-05-26 11:52:16 +0900

    20160526-2

--- 02/HomeOutput.php
+++ 02/HomeOutput.php
@@ -13,29 +13,45 @@
 
 <table class="type05">
     <tr><th>氏名</th><td><?php  echo $_POST['name1']." ".$_POST['name2'] ?></td></tr>
-    <tr><th>性別</th><td><?php  echo $_POST['sex'] ?></td></tr>
+    <tr><th>性別</th><td><?php
+                                if(isset($_POST['sex'])){
+                                    foreach ( $_POST['sex'] as $value) {
+                                        switch ($value) {
+                                            case '0':
+                                                echo "男性";
+                                                break;
+                                            case '1':
+                                                echo "女性";
+                                                break;
+                                            case '2':
+                                                echo "どちらでもない";
+                                                break;
+                                        }
+                                    }
+                                }
+?></td></tr> ?></td></tr>
     <tr><th>住所</th><td><?php  echo $_POST['address'] ?></td></tr>
     <tr><th>電話番号</th><td><?php  echo $_POST['tel']."-".$_POST['tel2']."-".$_POST['tel3'] ?></td></tr>
     <tr><th>メールアドレス</th><td><?php  echo $_POST['mail1']."@".$_POST['mail2'] ?></td></tr>
     <tr><th>どこで知ったか<br>（複数回答可）</th><td><?php
-    if(empty($_POST['where'])==false){
-        foreach ( $_POST['where'] as $value) {
-            switch ($value) {
-                case '0':
-                    echo "あっちの方".",";
-                    break;
-                case '1':
-                    echo "そっちの方";
-                    break;
-                case '2':
-                    echo "そっちの方";
-                    break;
-                case '3':
-                    echo "そっちの方";
-                    break;
-            }
-        }
-    }
+                                                    if(isset($_POST['where'])){
+                                                        foreach ( $_POST['where'] as $value) {
+                                                            switch ($value) {
+                                                                case '0':
+                                                                    echo "あっちの方だよ";
+                                                                    break;
+                                                                case '1':
+                                                                    echo "そっちの方だよ";
+                                                                    break;
+                                                                case '2':
+                                                                    echo "この辺だよ";
+                                                                    break;
+                                                                case '3':
+                                                                    echo "その他だよ";
+                                                                    break;
+                                                            }
+                                                        }
+                                                    }
     ?></td></tr>
     <tr><th>質問カテゴリー</th><td><?php
     if(isset( $_POST['category'] )){
