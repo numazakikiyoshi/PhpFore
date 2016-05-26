6611c9815fb7d9953cbef2fd250f04de4f7d8530
numazaki <numaki0110@yahoo.co.jp>
2016-05-26 12:14:14 +0900

    20160525-3

--- 02/HomeOutput.php
+++ 02/HomeOutput.php
@@ -14,22 +14,18 @@
 <table class="type05">
     <tr><th>氏名</th><td><?php  echo $_POST['name1']." ".$_POST['name2'] ?></td></tr>
     <tr><th>性別</th><td><?php
-                                if(isset($_POST['sex'])){
-                                    foreach ( $_POST['sex'] as $value) {
-                                        switch ($value) {
-                                            case '0':
-                                                echo "男性";
-                                                break;
-                                            case '1':
-                                                echo "女性";
-                                                break;
-                                            case '2':
-                                                echo "どちらでもない";
-                                                break;
-                                        }
-                                    }
+                                switch ($_POST['sex']) {
+                                    case '0':
+                                        echo "男性";
+                                        break;
+                                    case '1':
+                                        echo "女性";
+                                        break;
+                                    case '2':
+                                        echo "どちらでもない";
+                                        break;
                                 }
-?></td></tr> ?></td></tr>
+    ?></td></tr>
     <tr><th>住所</th><td><?php  echo $_POST['address'] ?></td></tr>
     <tr><th>電話番号</th><td><?php  echo $_POST['tel']."-".$_POST['tel2']."-".$_POST['tel3'] ?></td></tr>
     <tr><th>メールアドレス</th><td><?php  echo $_POST['mail1']."@".$_POST['mail2'] ?></td></tr>
@@ -54,12 +50,28 @@
                                                     }
     ?></td></tr>
     <tr><th>質問カテゴリー</th><td><?php
-    if(isset( $_POST['category'] )){
-        foreach ( $_POST['category'] as $value) {
-            echo "「".$value."」";
-        }
-    }
-    ?></select></p></td></tr>
+                                        if(isset( $_POST['category'] )){
+                                            foreach ( $_POST['category'] as $value) {
+                                                switch ($value) {
+                                                    case '0':
+                                                        echo "購入した商品について<br >";
+                                                        break;
+                                                    case '1':
+                                                        echo "店舗について<br >";
+                                                        break;
+                                                    case '2':
+                                                        echo "採用について<br >";
+                                                        break;
+                                                    case '3':
+                                                        echo "その他<br >";
+                                                        break;
+                                                    case '4':
+                                                        echo "それ以外<br >";
+                                                        break;
+                                                }
+                                            }
+                                        }
+    ?></select></td></tr>
     <tr><th>質問内容</th><td><?php  echo $_POST['comment'] ?></td></tr>
 </table>
 
