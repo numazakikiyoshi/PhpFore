6afce16b5447ea23a1beac5142b0ab00a4f97566
numazaki <numaki0110@yahoo.co.jp>
2016-05-27 17:47:45 +0900

    20160527メアドなど、入力時にエラーを出る項目を増やした。

--- 02/contact.php
+++ 02/contact.php
@@ -13,21 +13,22 @@
 <table class="type05">
   <tr><th>姓</th><td><input type="textbox" name="name1"size="10" required placeholder="苗字"></td></tr>
   <tr><th>名</th><td><input type="textbox" name="name2"size="10" required placeholder="名前"></td></tr>
-  <tr><th>性別</th><td><input type="radio" name="sex" value="0"> 男性
-                    　　<input type="radio" name="sex" value="1"> 女性
-                    　　<input type="radio" name="sex" value="2" checked> どちらでもない</td></tr>
+  <tr><th>性別</th><td><label><input type="radio" name="sex" value="0"> 男性</label>
+                    　　<label><input type="radio" name="sex" value="1"> 女性</label>
+                    　　<label><input type="radio" name="sex" value="2" checked> どちらでもない</label></td></tr>
   <tr><th>住所</th><td><input type="textbox" name="address"required placeholder="どこか"></td></tr>
   <tr><th>電話番号</th><td><input type="textbox" pattern="^[0-9]{2,4}" name="tel" size="4"　required placeholder="123">-
                           <input type="textbox" pattern="^[0-9]{2,4}" name="tel2" size="4"　required placeholder="456">-
                           <input type="textbox" pattern="^[0-9]{2,4}" name="tel3" size="4"　required placeholder="789"></td></tr>
   <tr><th>メールアドレス</th><td><input type="textbox" name="mail1" size="20" required placeholder="mail">@
-                              <input type="textbox"  name="mail2" size="20" required placeholder="address"></td></tr>
-  <tr><th>どこで知ったか<br>（複数回答可）</td><td><input type="checkbox" name="where[]" value="0">あの辺
-                                            　<input type="checkbox" name="where[]" value="1">その辺<br>
-                                             <input type="checkbox" name="where[]" value="2">この辺
-                                             <input type="checkbox" name="where[]" value="3">その他<br></td></tr>
-  <tr><th>質問カテゴリー</th><td><select name="category[]" size ="5" multiple="multiple" required>
-                               <option value="0"　selected>購入した商品について</option>
+                              <input type="textbox" onKeyup="this.value=this.value.replace(/[^0-9a-z]+\.[a-z]{2,3}$/,'')" pattern="[A-Za-z0-9.-]+\.[a-z]{2,3}$" name="mail2" size="20" required placeholder="address"></td></tr>
+  <tr><th>どこで知ったか<br>（複数回答可）</td><td><label><input type="checkbox" name="where[]" value="00">あの辺</label>
+                                             <label><input type="checkbox" name="where[]" value="01">その辺</label><br>
+                                             <label><input type="checkbox" name="where[]" value="02">この辺</label>
+                                             <label><input type="checkbox" name="where[]" value="03">その他<br></td></tr>
+  <tr><th>質問カテゴリー</th><td><select name="category[]" size ="1" required>
+                               <option value="100"　selected>さっさと選びやがってください。</option>
+                               <option value="0"　>購入した商品について</option>
                                <option value="1">店舗について</option>
                                <option value="2">採用について</option>
                                <option value="3">その他</option>
